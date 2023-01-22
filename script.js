5682a914acba1e57661d41780a4c2d13a84905c0
Julia Hytovska <mejulia716@gmail.com>
2023-01-23 12:34:36 +1300

    js homework

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("you can ignore single and double quotes like this:\\\'\\\'\\\"\\\"");
+
+
+let data=prompt("Enter your dat");
+
+let namee=prompt("Enter your name");
+alert(`your name is ${namee}`);
+
+let surname=prompt("Enter your surname");
+alert(`your surname is ${surname}`);
+
+let admin = confirm("are you admin");
+alert(`your name is ${namee} your surname is ${surname} admin status ${admin}`);
+
+
+
